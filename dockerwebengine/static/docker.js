8873eae907f7c04dcff2f32ea3c7e89bea801b57
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2019-11-05 09:20:59 +0530

    Flask Docker Web Engine

--- dockerwebengine/static/docker.js
+++ dockerwebengine/static/docker.js
@@ -1,10 +1,7 @@
+//Refresh Docker Summary
 $(document).ready(function(){
 
 	$('.refreshButton').on('click',function() {
-
-		//var image_id = $(this).attr('refresh_image_id');
-		//console.log(image_id);
-
 		req = $.ajax({
 			url : '/refresh',
 			type : 'POST'
@@ -27,3 +24,82 @@ $(document).ready(function(){
 
 	});
 });
+
+
+//Refresh Docker Image
+$(document).ready(function(){
+
+	$('.refreshButton-image').on('click',function() {
+		req = $.ajax({
+			url : '/refresh',
+			type : 'POST'
+		});
+
+		req.done(function(data){
+			$('#Show_Image_Count').fadeOut(1000);
+			$('#Show_Image_Count').text(data.imagerefresh).fadeIn(1000);
+
+		});
+
+	});
+});
+
+
+//Refresh Docker Container
+$(document).ready(function(){
+
+	$('.refreshButton-container').on('click',function() {
+		req = $.ajax({
+			url : '/refresh',
+			type : 'POST'
+		});
+
+		req.done(function(data){
+
+			$('#Show_Container_Count').fadeOut(1000);
+			$('#Show_Container_Count').text(data.containerrefresh).fadeIn(1000);
+			
+		});
+
+	});
+});
+
+//Refresh Docker Volume
+$(document).ready(function(){
+
+	$('.refreshButton-volume').on('click',function() {
+		req = $.ajax({
+			url : '/refresh',
+			type : 'POST'
+		});
+
+		req.done(function(data){
+			
+			$('#Show_Volume_Count').fadeOut(1000);
+			$('#Show_Volume_Count').text(data.volumerefresh).fadeIn(1000);
+
+			
+		});
+
+	});
+});
+
+
+//Refresh Docker Network
+$(document).ready(function(){
+
+	$('.refreshButton-network').on('click',function() {
+		req = $.ajax({
+			url : '/refresh',
+			type : 'POST'
+		});
+
+		req.done(function(data){
+			
+			$('#Show_Network_Count').fadeOut(1000);
+			$('#Show_Network_Count').text(data.networkrefresh).fadeIn(1000);
+		});
+
+	});
+});
+
